d6e337f9e490e1053b13791d09a40269a117b85e
Gabriel <gabrielpachecotec@gmail.com>
2024-09-20 09:00:49 +0100

    Add basic mathematical functions and operate()

---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(firstNum, operator, secondNum) {
+    switch (operator) {
+        case '+':
+            return add(firstNum, secondNum);
+        case '-':
+            return subtract(firstNum, secondNum);
+        case '*':
+            return multiply(firstNum, secondNum);
+        case '/':
+            return divide(firstNum, secondNum);
+    }
+}
+
+let firstNum, operator, secondNum;
